317e724e785f2e51a91a4351aeba850b45fc6070
Jesse Devaney <jessesdevaney@gmail.com>
2021-05-11 14:25:06 -0400

    Create and append svg to #root. Style svg.
    - Import d3 to create and manipulate the svg (app.js)
    - Set viewBox attr to make the svg responsive (app.js)
    - Create shadows under the svg with box-shadow (app.scss)
    - Center svg inside of #root (index.scss)
    - Paint the svg after the DOM has loaded (index.js)

---
+++ src/app/app.js
@@ -0,0 +1,13 @@
+import * as d3 from "d3";
+import "./app.scss";
+
+export default function createScatterPlot() {
+  const width = 800;
+  const height = 450;
+
+  const svg = d3
+    .select("#root")
+    .append("svg")
+    .attr("id", "chart")
+    .attr("viewBox", `0 0 ${width} ${height}`);
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./index.scss";
+import createScatterPlot from "./app/app";
+
+function ready() {
+  createScatterPlot();
+}
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
