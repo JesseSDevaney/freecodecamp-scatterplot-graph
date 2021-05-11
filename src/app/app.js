011c3b163c5d76e3e936a6a394699b6b5bb989e8
Jesse Devaney <jessesdevaney@gmail.com>
2021-05-11 15:04:31 -0400

    Create and style x-axis label
    - Append x-axis label text to SVG (app.js)
    - Add font for x-axis label (index.html, app.scss)
    - Adjust padHeight to make space for x-axis label

--- src/app/app.js
+++ src/app/app.js
@@ -12,20 +12,28 @@ export default function createScatterPlot(dataset) {
     .attr("viewBox", `0 0 ${width} ${height}`);
 
   const padWidth = 0.1 * width;
-  const padHeight = 0.1 * height;
+  const padHeight = 0.12 * height;
 
   // Create x-axis
   const years = dataset.map((d) => new Date(d["Year"], 0));
-
   const xScale = d3
     .scaleTime()
     .domain([d3.min(years), d3.max(years)])
     .range([padWidth, width - padWidth]);
-
   const xAxis = d3.axisBottom(xScale);
+
   svg
     .append("g")
     .call(xAxis)
     .attr("id", "x-axis")
     .attr("transform", `translate(0, ${height - padHeight})`);
+
+  // Label x-axis
+  svg
+    .append("text")
+    .text("Years")
+    .attr("id", "x-axis-label")
+    .attr("class", "axis-label")
+    .attr("x", "80%")
+    .attr("y", "97%");
 }
